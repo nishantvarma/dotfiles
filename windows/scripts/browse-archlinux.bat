d783adc85a05eaa0df704bed37e29454d9bca56b
Nishant Varma <nishant.varma@gmail.com>
2021-08-22 20:17:55 +0530

    Fixups

--- windows/scripts/browse-archlinux.bat
+++
@@ -1,3 +0,0 @@
-@rem vim: tw=158
-
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -app="http://archlinux"
--- windows/scripts/update-dns.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-pushd %~dp0
-python update-dns.py
-popd
-
-pause
