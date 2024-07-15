877a2698cfd422de035b92286b89d55d212370cf
Nishant Varma <tnishant@zeomega.com>
2024-07-15 12:23:34 +0530

    Fixups

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,15 @@
 vim.cmd("source $HOME/.unified/.unifiedrc")
 vim.cmd("source $HOME/.config/nvim/nvim.vim")
 
-local status, module = pcall(require, "oil")
-if status then
-  require("oil").setup()
-else
-  print("skipping package oil")
+local function package(name)
+  local status, module = pcall(require, name)
+  if status then
+    module.setup()
+  else
+    print("skipping package " .. name)
+  end
 end
+
+package("oil")
+package("outline")
+package("telescope") -- needs plenary
