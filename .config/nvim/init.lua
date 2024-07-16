0ba6082c527c51da13d0fc4c2972dadc13ff20c0
Nishant Varma <tnishant@zeomega.com>
2024-07-16 10:41:59 +0530

    Fixups

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,27 @@
 vim.cmd("source $HOME/.unified/.unifiedrc")
 vim.cmd("source $HOME/.config/nvim/nvim.vim")
 
-local function package(name)
+local function package(name, opts)
   local status, module = pcall(require, name)
   if status then
-    module.setup()
+    module.setup(opts or {})
   else
     print("skipping package " .. name)
   end
 end
 
+package("nvim-tree", {
+  renderer = {
+    icons = {
+      show = {
+        file = false,
+        folder = false,
+        folder_arrow = false,
+        git = false
+      }
+    }
+  }
+})
 package("oil")
 package("outline")
 package("telescope") -- needs plenary
