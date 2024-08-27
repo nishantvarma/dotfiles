afd7538a29d22695d78a7381815108068027daa6
Nishant Varma <tnishant@zeomega.com>
2024-08-27 16:46:40 +0530

    Fixups

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,6 +1,9 @@
 require("vis")
 
+vis:map(vis.modes.NORMAL, "s", ":w<Enter>")
+
 vis.events.subscribe(vis.events.INIT, function()
+    vis:command(string.format("open '%s'", "scratch/scratch.txt"))
     vis:command("set theme terminal")
 end)
 
