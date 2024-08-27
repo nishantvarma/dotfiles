69ad61451d4fe48f875e8c1895352d1b08f8ab99
Nishant Varma <tnishant@zeomega.com>
2024-08-27 17:42:22 +0530

    Fixups

--- .config/vis/themes/terminal.lua
+++ .config/vis/themes/terminal.lua
@@ -4,16 +4,16 @@ lexers.STYLE_ATTRIBUTE = "fore:green,bold"
 lexers.STYLE_CLASS = "fore:yellow,bold"
 lexers.STYLE_COMMENT = "fore:blue,bold"
 lexers.STYLE_CONSTANT = "fore:cyan,bold"
-lexers.STYLE_DEFAULT = ""
+lexers.STYLE_DEFAULT = "fore:white"
 lexers.STYLE_DEFINITION = "fore:blue,bold"
 lexers.STYLE_EMBEDDED = "back:gray,fore:white"
 lexers.STYLE_ERROR = "fore:red,italics"
 lexers.STYLE_FUNCTION = "fore:blue,bold"
 lexers.STYLE_HEADING = "fore:magenta"
-lexers.STYLE_IDENTIFIER = ""
+lexers.STYLE_IDENTIFIER = "fore:white"
 lexers.STYLE_KEYWORD = "fore:yellow,bold"
 lexers.STYLE_LABEL = "fore:green,bold"
-lexers.STYLE_NOTHING = ""
+lexers.STYLE_NOTHING = "fore:white"
 lexers.STYLE_NUMBER = "fore:red,bold"
 lexers.STYLE_OPERATOR = "fore:cyan,bold"
 lexers.STYLE_PREPROCESSOR = "fore:magenta,bold"
@@ -22,15 +22,15 @@ lexers.STYLE_STRING = "fore:red,bold"
 lexers.STYLE_TAG = "fore:red,bold"
 lexers.STYLE_TYPE = "fore:green,bold"
 lexers.STYLE_VARIABLE = "fore:blue,bold"
-lexers.STYLE_WHITESPACE = ""
+lexers.STYLE_WHITESPACE = "fore:white"
 
 lexers.STYLE_COLOR_COLUMN = "back:red"
 lexers.STYLE_CURSOR = "back:white,fore:black"
 lexers.STYLE_CURSOR_LINE = "underlined"
-lexers.STYLE_CURSOR_PRIMARY = lexers.STYLE_CURSOR..",fore:yellow"
-lexers.STYLE_EOF = ""
+lexers.STYLE_CURSOR_PRIMARY = lexers.STYLE_CURSOR .. ",fore:yellow"
+lexers.STYLE_EOF = "fore:white"
 lexers.STYLE_INFO = "bold"
-lexers.STYLE_LINENUMBER = ""
+lexers.STYLE_LINENUMBER = "fore:white"
 lexers.STYLE_LINENUMBER_CURSOR = lexers.STYLE_LINENUMBER
 lexers.STYLE_SELECTION = "back:white,bold"
 lexers.STYLE_SEPARATOR = lexers.STYLE_DEFAULT
@@ -46,8 +46,8 @@ lexers.STYLE_DELETION = "fore:red"
 -- CSS
 
 lexers.STYLE_PROPERTY = lexers.STYLE_ATTRIBUTE
-lexers.STYLE_PSEUDOCLASS = ""
-lexers.STYLE_PSEUDOELEMENT = ""
+lexers.STYLE_PSEUDOCLASS = "fore:white"
+lexers.STYLE_PSEUDOELEMENT = "fore:white"
 
 -- HTML
 
@@ -60,9 +60,9 @@ lexers.STYLE_TAG_UNKNOWN = lexers.STYLE_TAG .. ",italics"
 
 lexers.STYLE_BOLD = "bold"
 lexers.STYLE_CODE = lexers.STYLE_EMBEDDED
-lexers.STYLE_HR = ""
+lexers.STYLE_HR = "fore:white"
 for i = 1,6 do
-    lexers["STYLE_HEADING_H"..i] = lexers.STYLE_HEADING
+    lexers["STYLE_HEADING_H" .. i] = lexers.STYLE_HEADING
 end
 lexers.STYLE_ITALIC = "italics"
 lexers.STYLE_LIST = lexers.STYLE_KEYWORD
