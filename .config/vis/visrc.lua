f74192c0b0df4216aced7013c28f9e490ca886cd
Nishant Varma <tnishant@zeomega.com>
2024-08-28 09:16:51 +0530

    Fixups

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -5,13 +5,16 @@ vis:command_register("fzf", function(argv, force, win, selection, range)
     local file = io.popen(command)
     local output = file:read()
     local success, msg, status = file:close()
-
     vis:command(string.format("e '%s'", output))
     vis:feedkeys("<vis-redraw>")
 end)
 
-vis:map(vis.modes.NORMAL, "s", ":w<Enter>")
 vis:map(vis.modes.NORMAL, " ", ":<Up>")
+vis:map(vis.modes.NORMAL, "s", ":w<Enter>")
+vis:map(vis.modes.VISUAL, "gf", function()
+    local win = vis.win
+    local text = win.file:content(win.selection.range)
+end)
 
 vis.events.subscribe(vis.events.INIT, function()
     vis:command(string.format("open '%s'", "scratch/scratch.txt"))
