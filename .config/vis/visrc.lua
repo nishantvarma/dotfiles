dd089691842197248444c60cfc8f7038afd78c07
Nishant Varma <tnishant@zeomega.com>
2024-08-31 17:36:38 +0530

    Fixups

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,23 +1,29 @@
 require("vis")
 
-vis:command_register("fzf", function(argv, force, win, selection, range)
-    local command = "fzf"
+vis:command_register("execute", function(argv, force, win, selection, range)
+    local command = table.concat(argv, " ")
+    vis:info("executing " .. command)
     local file = io.popen(command)
     local output = file:read()
     local success, msg, status = file:close()
-    vis:command(string.format("e '%s'", output))
+    if output then
+        vis:command(output)
+    end
     vis:feedkeys("<vis-redraw>")
 end)
 
-vis:map(vis.modes.NORMAL, " ", ":<Up>")
+vis:map(vis.modes.NORMAL, " ", ":execute commands<Enter>")
 vis:map(vis.modes.NORMAL, "s", ":w<Enter>")
 vis:map(vis.modes.VISUAL, "gf", function()
     local win = vis.win
     local text = win.file:content(win.selection.range)
 end)
+vis:map(vis.modes.NORMAL, "\\\\", function()
+end)
+vis:map(vis.modes.NORMAL, "\\f", ":execute preview<Enter>")
+vis:map(vis.modes.NORMAL, "\\q", ":q<Enter>")
 
 vis.events.subscribe(vis.events.INIT, function()
-    vis:command(string.format("open '%s'", "scratch/scratch.txt"))
     vis:command("set theme terminal")
 end)
 
