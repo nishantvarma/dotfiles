6692516cedb6b92991d93fa0b6332d586ede74a9
Nishant Varma <tnishant@zeomega.com>
2024-08-31 18:05:27 +0530

    Fixups

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -12,16 +12,31 @@ vis:command_register("execute", function(argv, force, win, selection, range)
     vis:feedkeys("<vis-redraw>")
 end)
 
+-- Normal Keys
 vis:map(vis.modes.NORMAL, " ", ":execute commands<Enter>")
 vis:map(vis.modes.NORMAL, "s", ":w<Enter>")
+vis:map(vis.modes.VISUAL, "s", "<Escape>sgv")
+vis:map(vis.modes.VISUAL_LINE, "s", "<Escape>sgv")
+
+-- The G Key
 vis:map(vis.modes.VISUAL, "gf", function()
     local win = vis.win
     local text = win.file:content(win.selection.range)
 end)
+
+-- Leader Key
 vis:map(vis.modes.NORMAL, "\\\\", function()
 end)
 vis:map(vis.modes.NORMAL, "\\f", ":execute preview<Enter>")
 vis:map(vis.modes.NORMAL, "\\q", ":q<Enter>")
+
+-- Cut Copy Paste
+vis:map(vis.modes.NORMAL, "<C-c>", '"+yy')
+vis:map(vis.modes.VISUAL, "<C-c>", '"+ygv')
+vis:map(vis.modes.NORMAL, "<C-v>", '"+p')
+vis:map(vis.modes.VISUAL, "<C-v>", '"+p')
+vis:map(vis.modes.NORMAL, "<C-x>", '"+dd')
+vis:map(vis.modes.VISUAL, "<C-x>", '"+d')
 
 vis.events.subscribe(vis.events.INIT, function()
     vis:command("set theme terminal")
