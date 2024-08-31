3f03c24cb0cffd63a44397a0e3309c7861517ba0
Nishant Varma <tnishant@zeomega.com>
2024-08-31 21:25:17 +0530

    Fixups

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -19,16 +19,11 @@ vis:map(vis.modes.VISUAL, "gf", function()
     local text = win.file:content(win.selection.range)
 end)
 
-vis:map(vis.modes.NORMAL, "\\\\", "")
 vis:map(vis.modes.NORMAL, "\\f", ":execute preview<Enter>")
 vis:map(vis.modes.NORMAL, "\\q", ":q<Enter>")
 
-vis:map(vis.modes.NORMAL, "<C-c>", '"+yy')
-vis:map(vis.modes.VISUAL, "<C-c>", '"+ygv')
-vis:map(vis.modes.NORMAL, "<C-v>", '"+p')
-vis:map(vis.modes.VISUAL, "<C-v>", '"+p')
-vis:map(vis.modes.NORMAL, "<C-x>", '"+dd')
-vis:map(vis.modes.VISUAL, "<C-x>", '"+d')
+vis:map(vis.modes.NORMAL, "<Tab>", function()
+end)
 
 vis.events.subscribe(vis.events.INIT, function()
     vis:command("set theme terminal")
