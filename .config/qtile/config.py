80ef9c9b9cf0f548102c91ed065e1a3418117406
Nishant Varma <tnishant@zeomega.com>
2025-03-11 22:16:16 +0530

    Fixups

---
+++ .config/qtile/config.py
@@ -0,0 +1,82 @@
+from libqtile import bar, layout, widget
+from libqtile.config import Click, Drag, Group, Key, Screen
+from libqtile.lazy import lazy
+
+mod = "mod4"
+terminal = "st"
+
+auto_fullscreen = True
+auto_minimize = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+wl_input_rules = None
+wl_xcursor_theme = None
+wl_xcursor_size = 24
+
+keys = [
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window left"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window right"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload config"),
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle split layout"),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window left"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window right"),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle layouts"),
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
+    Key([mod], "h", lazy.layout.left(), desc="Move focus left"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus right"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset window sizes"),
+    Key([mod], "r", lazy.spawncmd(), desc="Run command"),
+    Key([mod], "space", lazy.layout.next(), desc="Move focus to next window"),
+    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating"),
+    Key([mod], "w", lazy.window.kill(), desc="Close focused window"),
+]
+
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+groups = [Group(i) for i in "123456789"]
+for i in groups:
+    keys.extend(
+        [
+            Key([mod], i.name, lazy.group[i.name].toscreen(), desc=f"Switch to group {i.name}"),
+            Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True), desc=f"Move window to group {i.name}"),
+        ]
+    )
+
+layouts = [layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4), layout.Max()]
+
+widget_defaults = dict(font="sans", fontsize=12, padding=3)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.CurrentLayout(),
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Chord(chords_colors={"launch": ("#ff0000", "#ffffff")}, name_transform=lambda name: name.upper()),
+                widget.TextBox("default config", name="default"),
+                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+                widget.Systray(),
+                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+                widget.QuickExit(),
+            ],
+            24,
+        )
+    )
+]
+
+floating_layout = layout.Floating(float_rules=[*layout.Floating.default_float_rules])
