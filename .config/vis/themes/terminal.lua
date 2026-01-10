ae0539d0e4c7fc41b877652c097fd4afccb51d1d
Nishant Thampuran <tnishant@zeomega.com>
2026-01-10 16:38:53 +0530

    Fixups

--- .config/vis/themes/terminal.lua
+++ .config/vis/themes/terminal.lua
@@ -1,70 +1,57 @@
 local lexers = vis.lexers
 
+-- ui
+lexers.STYLE_DEFAULT = ""
+lexers.STYLE_CURSOR = "fore:black,back:white"
+lexers.STYLE_CURSOR_PRIMARY = "fore:black,back:white"
+lexers.STYLE_CURSOR_LINE = ""
+lexers.STYLE_SELECTION = "fore:default,back:gray"
+lexers.STYLE_LINENUMBER = "fore:gray"
+lexers.STYLE_LINENUMBER_CURSOR = "fore:white"
 lexers.STYLE_COLOR_COLUMN = "back:red"
-lexers.STYLE_CURSOR = "back:gray,fore:default"
-lexers.STYLE_CURSOR_LINE = "underlined"
-lexers.STYLE_CURSOR_PRIMARY = "back:white,fore:black"
-lexers.STYLE_EOF = "fore:black"
-lexers.STYLE_INFO = "bold"
-lexers.STYLE_LINENUMBER = "fore:green"
-lexers.STYLE_LINENUMBER_CURSOR = "fore:red"
-lexers.STYLE_SELECTION = "back:gray,fore:default"
-lexers.STYLE_SEPARATOR = lexers.STYLE_DEFAULT
-lexers.STYLE_STATUS = "back:gray,fore:yellow"
-lexers.STYLE_STATUS_FOCUSED = "back:gray,fore:green"
+lexers.STYLE_SEPARATOR = "fore:gray"
+lexers.STYLE_STATUS = "fore:white,back:gray"
+lexers.STYLE_STATUS_FOCUSED = "fore:green,back:gray"
+lexers.STYLE_INFO = "fore:white"
+lexers.STYLE_EOF = "fore:gray"
 
-lexers.STYLE_ATTRIBUTE = "fore:green"
-lexers.STYLE_CLASS = "fore:yellow"
-lexers.STYLE_COMMENT = "fore:cyan"
-lexers.STYLE_CONSTANT = "fore:red"
-lexers.STYLE_DEFAULT = "fore:white"
-lexers.STYLE_DEFINITION = "fore:green"
-lexers.STYLE_EMBEDDED = "back:gray,fore:white"
-lexers.STYLE_ERROR = "back:red,fore:white"
-lexers.STYLE_FUNCTION = "fore:red"
-lexers.STYLE_HEADING = "fore:blue"
-lexers.STYLE_IDENTIFIER = "fore:white"
-lexers.STYLE_KEYWORD = "fore:magenta"
-lexers.STYLE_LABEL = "fore:green"
-lexers.STYLE_NOTHING = "fore:white"
-lexers.STYLE_NUMBER = "fore:cyan"
-lexers.STYLE_OPERATOR = "fore:cyan"
-lexers.STYLE_PREPROCESSOR = "fore:magenta"
-lexers.STYLE_REGEX = "fore:red"
+-- syntax
+lexers.STYLE_COMMENT = "fore:gray"
 lexers.STYLE_STRING = "fore:green"
+lexers.STYLE_REGEX = "fore:green"
+lexers.STYLE_NUMBER = "fore:cyan"
+lexers.STYLE_CONSTANT = "fore:cyan"
+lexers.STYLE_KEYWORD = "fore:yellow"
+lexers.STYLE_TYPE = "fore:cyan"
+lexers.STYLE_FUNCTION = "fore:blue"
+lexers.STYLE_CLASS = "fore:yellow"
+lexers.STYLE_DEFINITION = "fore:blue"
+lexers.STYLE_VARIABLE = ""
+lexers.STYLE_IDENTIFIER = ""
+lexers.STYLE_OPERATOR = ""
+lexers.STYLE_PREPROCESSOR = "fore:magenta"
+lexers.STYLE_ATTRIBUTE = "fore:cyan"
+lexers.STYLE_LABEL = "fore:magenta"
 lexers.STYLE_TAG = "fore:yellow"
-lexers.STYLE_TYPE = "fore:green"
-lexers.STYLE_VARIABLE = "fore:blue"
-lexers.STYLE_WHITESPACE = "fore:white"
+lexers.STYLE_ERROR = "fore:white,back:red"
+lexers.STYLE_EMBEDDED = "fore:green"
+lexers.STYLE_NOTHING = ""
+lexers.STYLE_WHITESPACE = ""
+lexers.STYLE_HEADING = "fore:blue,bold"
 
--- Diff
-
+-- diff
 lexers.STYLE_ADDITION = "fore:green"
-lexers.STYLE_CHANGE = "fore:yellow"
 lexers.STYLE_DELETION = "fore:red"
+lexers.STYLE_CHANGE = "fore:yellow"
 
--- CSS
-
-lexers.STYLE_PROPERTY = lexers.STYLE_ATTRIBUTE
-lexers.STYLE_PSEUDOCLASS = "fore:white"
-lexers.STYLE_PSEUDOELEMENT = "fore:white"
-
--- HTML
-
-lexers.STYLE_ATTRIBUTE_UNKNOWN = lexers.STYLE_ATTRIBUTE .. ",italics"
-lexers.STYLE_TAG_DOCTYPE = lexers.STYLE_TAG
-lexers.STYLE_TAG_SINGLE = lexers.STYLE_TAG
-lexers.STYLE_TAG_UNKNOWN = lexers.STYLE_TAG .. ",italics"
-
--- Markdown
-
+-- markup
 lexers.STYLE_BOLD = "bold"
-lexers.STYLE_CODE = "fore:white"
-lexers.STYLE_HR = "fore:white"
-for i = 1,6 do
+lexers.STYLE_ITALIC = "italics"
+lexers.STYLE_CODE = "fore:green"
+lexers.STYLE_LINK = "fore:cyan"
+lexers.STYLE_REFERENCE = "fore:cyan"
+lexers.STYLE_HR = "fore:gray"
+lexers.STYLE_LIST = "fore:yellow"
+for i = 1, 6 do
     lexers["STYLE_HEADING_H" .. i] = lexers.STYLE_HEADING
 end
-lexers.STYLE_ITALIC = "italics"
-lexers.STYLE_LIST = lexers.STYLE_KEYWORD
-lexers.STYLE_LINK = lexers.STYLE_KEYWORD
-lexers.STYLE_REFERENCE = lexers.STYLE_KEYWORD
